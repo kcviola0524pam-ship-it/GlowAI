27ea3766c0ee634cbe2e0b242871dcd4566bf942
kcviola0524pam-ship-it <kcviola0524pam@student.fatima.edu.ph>
2026-05-13 02:55:34 +0800

    fix

--- spa-management-system/server/db.js
+++ spa-management-system/server/db.js
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
 import mysql from 'mysql2'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
